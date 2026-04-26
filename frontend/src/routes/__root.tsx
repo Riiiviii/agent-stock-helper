87e9c0b5d21dece885747a6cc6e839e0baeada9a
Gabriel Riven Wahnich <riven.development@gmail.com>
2026-04-26 14:49:31 +1000

    fix: fragments should contain more than one child

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -3,7 +3,9 @@ import { createRootRoute, Outlet } from "@tanstack/react-router";
 export const Route = createRootRoute({
   component: () => (
     <>
+      <nav></nav>
       <Outlet />
+      <footer />
     </>
   ),
 });

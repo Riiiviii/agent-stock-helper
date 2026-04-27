93ee665ff59591e79ef236589fc44e9cf3045bd2
Gabriel Riven Wahnich <riven.development@gmail.com>
2026-04-27 11:14:10 +1000

    fix: add tailwind css to vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,9 +2,11 @@ import { fileURLToPath } from 'url'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     tanstackRouter({
       target: 'react',
       autoCodeSplitting: true,

a8e38aa4a857a242dbc8d527bf1deb9d273a6d5f
Gabriel Riven Wahnich <riven.development@gmail.com>
2026-04-27 12:41:41 +1000

    feat: add image background for hero section
    feat: add page templates
    
    feat: add landing page component

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -5,5 +5,14 @@ export const Route = createFileRoute("/")({
 });
 
 function RouteComponent() {
-  return <div>Hello!</div>;
+  return (
+    <div className="absolute inset-0 -z-10">
+      <img
+        className="object-cover w-full h-full"
+        src="https://images.unsplash.com/photo-1611974789855-9c2a0a7236a3"
+        alt=""
+      />
+      <div className="absolute inset-0 bg-[#080e0c] opacity-94" />
+    </div>
+  );
 }

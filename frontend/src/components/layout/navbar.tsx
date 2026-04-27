e32ede44ccb3e10ebd5dbada3d3f9452a4cd5f96
Gabriel Riven Wahnich <riven.development@gmail.com>
2026-04-27 12:14:24 +1000

    feat: add styling for logo, navbar and signup buttons

--- frontend/src/components/layout/navbar.tsx
+++ frontend/src/components/layout/navbar.tsx
@@ -1,10 +1,14 @@
+import { Link } from "@tanstack/react-router";
 import NavbarLink from "./navbar-link";
+import { Button } from "../ui/button";
 
 export default function Navbar() {
   return (
-    <nav className="flex justify-between items-center bg-transparent">
+    <nav className="flex justify-between items-center bg-transparent mt-5 mb-5 w-full px-16">
       <div>
-        <h1>VESTLY</h1>
+        <Button variant="logo" render={<Link to="/" />}>
+          V E S T L Y
+        </Button>
       </div>
       <div className="flex items-center">
         <NavbarLink href="/" label="Home" />
@@ -13,7 +17,7 @@ export default function Navbar() {
         <NavbarLink href="/" label="Contact" />
         <div className="w-px h-4 bg-border mx-2" />
         <NavbarLink href="/" label="Log in" />
-        <NavbarLink href="/" label="Sign up" />
+        <NavbarLink href="/" label="Sign up" variant="signup" />
       </div>
     </nav>
   );
--- frontend/src/components/ui/button.tsx
+++ frontend/src/components/ui/button.tsx
@@ -1,7 +1,7 @@
-import { Button as ButtonPrimitive } from "@base-ui/react/button"
-import { cva, type VariantProps } from "class-variance-authority"
+import { Button as ButtonPrimitive } from "@base-ui/react/button";
+import { cva, type VariantProps } from "class-variance-authority";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
   "group/button inline-flex shrink-0 items-center justify-center rounded-lg border border-transparent bg-clip-padding text-sm font-medium whitespace-nowrap transition-all outline-none select-none focus-visible:border-ring focus-visible:ring-3 focus-visible:ring-ring/50 active:not-aria-[haspopup]:translate-y-px disabled:pointer-events-none disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-3 aria-invalid:ring-destructive/20 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
@@ -18,6 +18,11 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive/10 text-destructive hover:bg-destructive/20 focus-visible:border-destructive/40 focus-visible:ring-destructive/20 dark:bg-destructive/20 dark:hover:bg-destructive/30 dark:focus-visible:ring-destructive/40",
         link: "text-primary underline-offset-4 hover:underline",
+        navbar:
+          "bg-transparent text-white font-light hover:opacity-60 transition-opacity duration-200 text-xs ",
+        signup:
+          "bg-transparent text-(--green-light) border border-white hover:bg-white/10 hover:text-white text-xs hover:border-white transition-colors duration-200 rounded-full",
+        logo: "font-serif font-semibold text-2xl bg-transparent text-white",
       },
       size: {
         default:
@@ -37,8 +42,8 @@ const buttonVariants = cva(
       variant: "default",
       size: "default",
     },
-  }
-)
+  },
+);
 
 function Button({
   className,
@@ -52,7 +57,7 @@ function Button({
       className={cn(buttonVariants({ variant, size, className }))}
       {...props}
     />
-  )
+  );
 }
 
-export { Button, buttonVariants }
+export { Button, buttonVariants };

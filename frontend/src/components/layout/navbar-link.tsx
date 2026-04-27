e8c88a4e8e9382704d8aa9e1586ff0d4d70c1626
Gabriel Riven Wahnich <riven.development@gmail.com>
2026-04-27 12:17:00 +1000

    feat: add variant prop to NavbarLink component

--- frontend/src/components/layout/navbar-link.tsx
+++ frontend/src/components/layout/navbar-link.tsx
@@ -1,15 +1,24 @@
 import { Link } from "@tanstack/react-router";
-import { Button } from "../ui/button";
+import { Button, buttonVariants } from "../ui/button";
+import { type VariantProps } from "class-variance-authority";
 
 interface NavbarLinkProp {
   href: string;
   label: string;
   exact?: boolean;
+  variant?: VariantProps<typeof buttonVariants>["variant"];
 }
 
-export default function NavbarLink({ href, label, exact }: NavbarLinkProp) {
+export default function NavbarLink({
+  href,
+  label,
+  exact,
+  variant = "default",
+}: NavbarLinkProp) {
   return (
     <Button
+      className="ml-1 mr-1"
+      variant={variant}
       render={<Link to={href} activeOptions={{ exact: exact ?? false }} />}
     >
       {label}

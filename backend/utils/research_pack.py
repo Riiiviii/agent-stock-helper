74f2d41bf884863dc17c571d2b9ea05b25170265
Gabriel Riven Wahnich <riven.development@gmail.com>
2026-04-28 15:52:06 +1000

    feat: add logic in obtaining company summary text

--- backend/utils/research_pack.py
+++ backend/utils/research_pack.py
@@ -2,9 +2,12 @@ from .types import DeducedMCP, ResearchPack
 
 
 def build_research_pack(data: DeducedMCP) -> ResearchPack:
+    company_summary: str = get_company_summary(
+        data["clean_data"]["company_information"]
+    )
 
     return {
-        "company_summary": "",
+        "company_summary": company_summary,
         "company_snapshot": {},
         "financial_snapshot": {},
         "price_movement": {},
@@ -14,8 +17,8 @@ def build_research_pack(data: DeducedMCP) -> ResearchPack:
     }
 
 
-def generate_company_summary(company_info: dict) -> str:
-    return ""
+def get_company_summary(company_info: dict) -> str:
+    return company_info.get("longBusinessSummary", "")
 
 
 def generate_company_snapshot(company_info: dict) -> dict:
